4b2bfa4ada5f9a5c2811652ae7282303c388b8b7
TruongTam <you@example.com>
2020-07-03 12:03:34 +0700

    push

---
+++ add.php
@@ -0,0 +1,18 @@
+<?php
+include_once("inc.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="get">
+    <input type="number" name="n">
+    <input type="number" name="m">
+    <button type="submit">gửi</button>
+    </form>
+</body>
+</html>
